ac3d188e4c13755643ace24ced9e4bf2567b34eb
Mark Tiedemann <www.marktiedemann@gmail.com>
2018-01-23 19:33:39 +0100

    Remove GA, format with prettier, add avatar image

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
   var body = document.querySelector('body')
   var mode = document.querySelector('.mode')
   var anchors = document.querySelectorAll('a')
@@ -10,27 +10,37 @@ document.addEventListener('DOMContentLoaded', function () {
   var moon = "<i class='fa fa-moon-o' style='" + moonAdjust + "'></i>"
   var sun = "<i class='fa fa-sun-o' style='" + sunAdjust + "'></i>"
 
-  function toggleDarkMode () {
+  function toggleDarkMode() {
     body.classList.add('dark')
     mode.classList.add('dark')
-    anchors.forEach(function (a) { a.classList.add('dark') })
+    anchors.forEach(function(a) {
+      a.classList.add('dark')
+    })
     accent.style.fontWeight = 400
     mode.innerHTML = sun
-    try { window.localStorage.setItem('mode', 'dark') } catch (e) {}
+    try {
+      window.localStorage.setItem('mode', 'dark')
+    } catch (e) {}
   }
 
-  function toggleLightMode () {
+  function toggleLightMode() {
     body.classList.remove('dark')
     mode.classList.remove('dark')
-    anchors.forEach(function (a) { a.classList.remove('dark') })
+    anchors.forEach(function(a) {
+      a.classList.remove('dark')
+    })
     accent.style.fontWeight = 500
     mode.innerHTML = moon
-    try { window.localStorage.setItem('mode', 'light') } catch (e) {}
+    try {
+      window.localStorage.setItem('mode', 'light')
+    } catch (e) {}
   }
 
-  try { if (window.localStorage.getItem('mode') === 'dark') toggleDarkMode() } catch (e) {}
+  try {
+    if (window.localStorage.getItem('mode') === 'dark') toggleDarkMode()
+  } catch (e) {}
 
-  mode.addEventListener('click', function () {
+  mode.addEventListener('click', function() {
     if (body.classList.contains('dark')) toggleLightMode()
     else toggleDarkMode()
   })
