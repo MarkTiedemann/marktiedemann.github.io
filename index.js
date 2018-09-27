502262f7e5c789312312ec251853f19b452e567b
Mark Tiedemann <www.marktiedemann@gmail.com>
2018-09-27 16:03:18 +0200

    Fix formatting of git date

--- index.js
+++ index.js
@@ -81,21 +81,57 @@ function renderCommit(commit) {
   if (commit.author.login !== 'MarkTiedemann') {
     commitEmail.textContent = '<unknown>'
   }
-  commitDate.textContent = formatGitDate(commit.commit.committer.date)
+  commitDate.textContent = formatGitDate(new Date(commit.commit.committer.date))
 }
 
-function formatGitDate(dateString) {
-  let date = new Date(dateString)
-  let formatter = new Intl.DateTimeFormat('en-US', {
-    weekday: 'short',
-    month: 'short',
-    day: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-    hour12: false,
-    year: 'numeric',
-    timeZoneName: 'short'
-  })
-  return formatter.format(date)
+/**
+ * Formats a JavaScript date (e.g. `2018-09-26T18:31:08.000Z`)
+ * into a Git date (e.g. `Wed Sep 26 20:31:08 2018 +0200`).
+ * @param {Date} date
+ * @returns {string}
+ */
+function formatGitDate(date) {
+  let weekday_dayInMonth_month_year = date.toDateString()
+  let weekday_dayInMonth_month = weekday_dayInMonth_month_year.replace(/ \d+$/, '')
+  let hours = leftPad(date.getHours().toString(), 2, '0')
+  let minutes = leftPad(date.getMinutes().toString(), 2, '0')
+  let seconds = leftPad(date.getSeconds().toString(), 2, '0')
+  let time = hours + ':' + minutes + ':' + seconds
+  let year = date.getFullYear()
+  let offset = formatGitTimezoneOffset(date.getTimezoneOffset())
+  return [weekday_dayInMonth_month, time, year, offset].join(' ')
+}
+
+/**
+ * Formats a JavaScript timezone offset in minutes (e.g `-120`)
+ * into a Git timezone offset (e.g. `+0200`).
+ * @param {number} offsetInMinutes
+ * @returns {string}
+ */
+function formatGitTimezoneOffset(offsetInMinutes) {
+  let offsetInHours = Math.abs(Math.round(offsetInMinutes / 60)).toString()
+  let offsetInHoursPadded = leftPad(offsetInHours, 2, '0')
+  let offsetRemainder = (offsetInMinutes % 60).toString()
+  let offsetRemainderPadded = leftPad(offsetRemainder, 2, '0')
+  let prefix = offsetInMinutes > 0 ? '-' : '+'
+  return prefix + offsetInHoursPadded + offsetRemainderPadded
+}
+
+/**
+ * Left pad the given string with a padding string to the specified length.
+ * @param {string} string
+ * @param {number} length
+ * @param {string} padString
+ * @returns {string}
+ */
+function leftPad(string, length, padString) {
+  if (string.padStart !== undefined) {
+    // ES2017
+    return string.padStart(length, padString)
+  } else {
+    while (string.length < length) {
+      string = padString + string
+    }
+    return string
+  }
 }
