bac8619fbc195f79bdfcb73aa314422f0e7f1898
Mark Tiedemann <www.marktiedemann@gmail.com>
2018-09-28 18:11:40 +0200

    Add basic linked data

--- build.ps1
+++ build.ps1
@@ -1,11 +1,11 @@
 $ErrorActionPreference = 'Stop'
 $Env:Path += ';node_modules/.bin'
 
-# Format *.{css,ts,md}
+# Format *.{css,ts,md,json}
 
 $stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
-prettier --config .prettierrc --loglevel silent --write '*.{css,ts,md}'
-Write-Host -Object "format *.{css,ts,md} $($stopwatch.ElapsedMilliseconds)ms"
+prettier --config .prettierrc --loglevel silent --write '*.{css,ts,md,json}'
+Write-Host -Object "format *.{css,ts,md,json} $($stopwatch.ElapsedMilliseconds)ms"
 
 # Build index.html
 
@@ -22,15 +22,17 @@ function Inline($openTag, $content, $closeTag) {
 
 $main = md2html index.md
 $prevCommit = (git log -1 --pretty=format:"%H~%cn~%ce~%cd") -split '~'
+$linkedData = Get-Content -Path linked_data.json -Raw | jq -c .
 
 $indexHtml = Inline '<main>' $main '</main>'
 $indexHtml = Inline '<span id="commit_hash">' $prevCommit[0] '</span>'
 $indexHtml = Inline '<span id="commit_author">' $prevCommit[1] '</span>'
 $indexHtml = Inline '<span id="commit_email">' ('&lt;' + $prevCommit[2] + '&gt;') '</span>'
 $indexHtml = Inline '<span id="commit_date">' $prevCommit[3] '</span>'
+$indexHtml = Inline '<script type="application/ld+json">' $linkedData '</script>'
 
 Set-Content -Path index.html -Value $indexHtml -NoNewline
-js-beautify --config .jsbeautifyrc --type html --quiet --replace index.html
+js-beautify --config .jsbeautifyrc --type html --unformatted script --quiet --replace index.html
 Write-Host -Object "build index.html $($stopwatch.ElapsedMilliseconds)ms"
 
 # Build *.ts
