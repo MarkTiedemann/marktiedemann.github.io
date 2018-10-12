520138700f9217f4870d2e52e46bbef791672ef2
Mark Tiedemann <www.marktiedemann@gmail.com>
2018-10-12 02:32:01 +0200

    Refactor build

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 ### [marksweb.site](https://marksweb.site)
 
-This simple static site was developed with a little bit of HTML, CSS and [TypeScript](https://www.typescriptlang.org/), and the cross-platform [PowerShell Core](https://github.com/powershell/powershell) for some minimal build scripts (there's no fancy webpack configuration here... _bummer, I know!_).
+This simple static site was developed with a little bit of HTML, CSS and [TypeScript](https://www.typescriptlang.org/).
 
-The PowerShell scripts execute some [Node.js](https://nodejs.org) CLI apps for tasks, such as, TypeScript compilation, JavaScript minification, code formatting, Markdown to HTML conversion, and other things.
+The build script is written in [PowerShell Core](https://github.com/powershell/powershell), but executes some [Node](https://nodejs.org) command-line apps for tasks, such as, TypeScript compilation, JavaScript minification, code formatting, Markdown to HTML conversion, and other things.
 
-To get started, you'll need to run `npm install` or `yarn install` to install the development dependencies. With `pwsh`, you can then run [`.\build.ps1`](build.ps1) to build the site and [`.\serve.ps1`](serve.ps1) to start a server and open the site in your browser.
+To get started, you'll need to run `yarn install` to install the development dependencies. In `pwsh`, you can then run [`.\build.ps1`](build.ps1) to incrementally (re-)build the site. To check out the site, simply open the `index.html` file in your browser.
 
 Issues, PRs and feedback of any kind are welcome!
 
--- build.ps1
+++ build.ps1
@@ -1,11 +1,33 @@
 $ErrorActionPreference = 'Stop'
 $Env:Path += ';node_modules/.bin'
 
-$stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
+prettier-if-modified '*.{css,ts,md,json}' -- prettier --write | ForEach-Object {
+  $file = $_.Split(' ')[0]
+  Write-Host "formatted $file"
+}
 
-prettier-if-modified '*.{css,ts,md,json}' -- prettier --write
+function Hash($text) {
+  $sha256 = New-Object System.Security.Cryptography.SHA256Managed
+  $textBytes = [System.Text.Encoding]::UTF8.GetBytes($text)
+  $hashBytes = $sha256.ComputeHash($textBytes)
+  foreach ($byte in $hashBytes) {
+    $hash += $byte.ToString()
+  }
+  $hash
+}
+
+if (Test-Path -Path .lastwrites) {
+  $lastWrites = Get-Content -Path .lastwrites -Raw
+  $lastWritesHash = Hash $lastWrites
+  $lastWrites = $lastWrites | ConvertFrom-Json
+}
+else {
+  $lastWrites = @{}
+  $lastWritesHash = $null
+}
 
 $indexHtml = Get-Content -Path index.html -Raw
+$indexHtmlHash = Hash $indexHtml
 
 function Inline($openTag, $content, $closeTag) {
   $openIndex = $indexHtml.IndexOf($openTag)
@@ -15,24 +37,84 @@ function Inline($openTag, $content, $closeTag) {
   $preData + $openTag + $content + $postData
 }
 
-$css = cleancss index.css
-$main = marked index.md
-$prevCommit = (git log -1 --pretty=format:"%H~%cn~%ce~%cd") -split '~'
-$linkedData = Get-Content -Path linked_data.json -Raw | ConvertFrom-Json | ConvertTo-Json -Compress
-tsc --project tsconfig.json
-$js = terser --compress --mangle --enclose --ecma 6 -- commit_log.js mode_toggle.js
+function Build($files, $command) {
+  $shouldBuild = @()
+  $shouldRebuild = @()
+  foreach ($file in $files) {
+    $lastRecordedWrite = $lastWrites.$file
+    $actualLastWrite = (Get-Item -Path $file).LastWriteTime
+    $lastWrites | Add-Member -NotePropertyName $file -NotePropertyValue $actualLastWrite -Force
+    if ($null -ne $lastRecordedWrite) {
+      $lastRecordedWrite = Get-Date -Date $lastRecordedWrite
+      if ($lastRecordedWrite -ne $actualLastWrite) {
+        $shouldRebuild += $file
+      }
+    }
+    else {
+      $shouldBuild += $file
+    }
+  }
+  if ($shouldBuild.Length -gt 0 -or $shouldRebuild.Length -gt 0) {
+    $result = & $command
+    foreach ($build in $shouldBuild) {
+      Write-Host "built $build"
+    }
+    foreach ($rebuild in $shouldRebuild) {
+      Write-Host "rebuilt $rebuild"
+    }
+    $result
+  }
+  else {
+    $indexHtml
+  }
+}
 
-$indexHtml = Inline '<style>' $css '</style>'
-$indexHtml = Inline '<main>' $main '</main>'
-$indexHtml = Inline '<span id="commit_hash">' $prevCommit[0] '</span>'
-$indexHtml = Inline '<span id="commit_author">' $prevCommit[1] '</span>'
-$indexHtml = Inline '<span id="commit_email">' ('&lt;' + $prevCommit[2] + '&gt;') '</span>'
-$indexHtml = Inline '<span id="commit_date">' $prevCommit[3] '</span>'
-$indexHtml = Inline '<script type="text/javascript">' $js '</script>'
-$indexHtml = Inline '<script type="application/ld+json">' $linkedData '</script>'
+$indexHtml = Build @('index.css') {
+  $css = cleancss index.css
+  Inline '<style>' $css '</style>'
+}
 
-Set-Content -Path index.html -Value $indexHtml -NoNewline
-js-beautify --config .jsbeautifyrc --type html --unformatted style `
-  --unformatted script --quiet --replace index.html
+$indexHtml = Build @('index.md') {
+  $main = marked index.md
+  Inline '<main>' $main '</main>'
+}
 
-Write-Host -Object "Done in $($stopwatch.ElapsedMilliseconds)ms."
+$indexHtml = Build @('ld.json') {
+  $ld = Get-Content -Path ld.json -Raw | ConvertFrom-Json | ConvertTo-Json -Compress
+  Inline '<script type="application/ld+json">' $ld '</script>'
+}
+
+Build @('commit_log.ts', 'mode_toggle.ts') {
+  tsc
+} | Out-Null
+
+$indexHtml = Build @('commit_log.js', 'mode_toggle.js') {
+  $js = terser --compress --mangle --enclose --ecma 5 -- commit_log.js mode_toggle.js
+  Inline '<script type="text/javascript">' $js '</script>'
+}
+
+$commit = (git log -1 --pretty=format:'%H~%cn~%ce~%cd') -split '~'
+$indexHtml = Inline '<span id="commit_hash">' $commit[0] '</span>'
+$indexHtml = Inline '<span id="commit_author">' $commit[1] '</span>'
+$indexHtml = Inline '<span id="commit_email">' ('&lt;' + $commit[2] + '&gt;') '</span>'
+$indexHtml = Inline '<span id="commit_date">' $commit[3] '</span>'
+
+if ($indexHtmlHash -ne (Hash $indexHtml)) {
+  Set-Content -Path index.html -Value $indexHtml -NoNewline
+  Write-Host "rebuilt index.html"
+  js-beautify --config .jsbeautifyrc --type html --unformatted style `
+    --unformatted script --quiet --replace index.html
+  Write-Host "formatted index.html"
+}
+
+$lastWrites = $lastWrites | ConvertTo-Json
+if ($null -ne $lastWritesHash) {
+  if ($lastWritesHash -ne (Hash $lastWrites)) {
+    Set-Content -Path .lastwrites -Value $lastWrites -NoNewline
+    Write-Host "rebuilt .lastwrites"
+  }
+}
+else {
+  Set-Content -Path .lastwrites -Value $lastWrites -NoNewline
+  Write-Host "built .lastwrites"
+}
